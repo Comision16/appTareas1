2339cc07c1ef0641646b230ea7a81c3072bae458
EricM76 <menaericdaniel@gmail.com>
2022-05-26 16:37:34 -0300

    structure initial

---
+++ app.js
@@ -0,0 +1,14 @@
+const {argv} = require('process');
+const tareas = require('./tareas')
+
+
+const comando = argv[2];
+
+switch (comando) {
+    case list:
+        console.log(tareas.listar);
+        break;
+
+    default:
+        break;
+}
---
+++ tareas.js
@@ -0,0 +1,5 @@
+module.exports = {
+    listar : () => {
+        return "lista de tareas"
+    }
+}
